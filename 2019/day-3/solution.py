442376366612eefd3bc494393431d030abcf0188
Nate Eagleson <neagleson@nxtbookmedia.com>
2019-12-09 08:26:16 -0500

    Progress on day 3 2019

--- 2019/day-3/solution.py
+++ 2019/day-3/solution.py
@@ -31,6 +31,7 @@ def parse_path(input_line):
 
     return points
 
+
 def parse_input(input_path):
     paths = []
 
@@ -41,15 +42,87 @@ def parse_input(input_path):
     return paths
 
 
+def between(n, left, right):
+    """Return True if 'n' is between 'left' and 'right', else return False."""
+
+    return ((left <= n <= right) or (left >= n >= right))
+
+
+def find_intersection(left_segment, right_segment):
+    left_seg_horizontal = left_segment[0][0] == left_segment[1][0]
+    left_seg_vertical = not left_seg_horizontal
+    right_seg_horizontal = right_segment[0][0] == right_segment[1][0]
+    right_seg_vertical = not right_seg_horizontal
+
+    if left_seg_horizontal:
+        if right_seg_vertical:
+            if (between(right_segment[0][0], left_segment[0][0], left_segment[1][0]) and
+                between(left_segment[0][1], right_segment[0][1], right_segment[1][1])):
+                return (right_segment[0][0], left_segment[0][1])
+        elif right_seg_horizontal:
+            if (between(right_segment[0][0], left_segment[0][0], left_segment[1][0]) and
+                between(left_segment[0][1], right_segment[0][1], right_segment[1][1])):
+                print('Parallel overlapping lines. Implement.')
+                sys.exit(1)
+    elif left_seg_vertical:
+        if right_seg_vertical:
+            if (between(right_segment[0][0], left_segment[0][0], left_segment[1][0]) and
+                between(left_segment[0][1], right_segment[0][1], right_segment[1][1])):
+                print('Parallel overlapping lines. Implement.')
+                sys.exit(1)
+        elif right_seg_horizontal:
+            if (between(right_segment[0][1], left_segment[0][1], left_segment[1][1]) and
+                between(left_segment[0][0], right_segment[0][0], right_segment[1][0])):
+                return (right_segment[0][0], left_segment[0][1])
+
+
+def find_intersections(left_path, right_path):
+    intersections = []
+
+    for i in range(0, len(left_path) - 1):
+        left_path_segment = (left_path[i], left_path[i + 1])
+        for j in range(0, len(right_path) - 1):
+            right_path_segment = (right_path[j], right_path[j + 1])
+
+            intersection = find_intersection(left_path_segment,
+                                             right_path_segment)
+
+            if intersection is not None:
+                intersections.append(intersection)
+
+    return intersections
+
+
+def get_closest_distance(intersections):
+    closest_distance = None
+
+    for intersection in intersections:
+        if intersection[0] == 0 and intersection[1] == 0:
+            continue
+
+        distance = intersection[0] + intersection[1]
+
+        if closest_distance is None or distance < closest_distance:
+            closest_distance = distance
+
+    return closest_distance
+
+
 def main(input_path):
     # TODO Implement this
     path_one, path_two = parse_input(input_path)
 
-    print(path_one, path_two); sys.exit()
+    print(path_one, path_two)
 
-    intersection = find_intersection(path_one, path_two)
+    intersections = find_intersections(path_one, path_two)
 
-    distance = compute_distance(intersection)
+    print(intersections)
+
+    distance = get_closest_distance(intersections)
+
+    if distance is None:
+        print('No intersections found. Go fix your bugs. :(')
+        sys.exit()
 
     print(distance)
 
