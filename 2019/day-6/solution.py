36e4394e57102757dd9efe02c0a34066296eeb0c
Nate Eagleson <neagleson@nxtbookmedia.com>
2019-12-16 23:30:09 -0500

    Solve part two of day 6, 2019
    Easy problems are nice.

--- 2019/day-6/solution.py
+++ 2019/day-6/solution.py
@@ -29,6 +29,45 @@ class TreeNode:
             return self.parent.get_depth() + 1
 
 
+    def contains(self, node):
+        # This approach only works if all TreeNodes in a program are
+        # singletons. True here but unwise generally - use unique IDs for a
+        # generic data structure.
+        if node is self:
+            return True
+
+        if node in self.children:
+            return True
+
+        for child in self.children:
+           if child.contains(node):
+               return True
+
+        # When there are no children we'll fall through to here.
+
+        return False
+
+
+    # REFACTOR Merge this with get_depth(), where ancestor is optional and
+    # defaults to None, so by default you get depth from root.
+    def depth(self, ancestor):
+        depth = 0
+
+        # As elsewhere, object identity is only correct if TreeNodes are
+        # singletons. They are in this case, though.
+        #
+        # Also kinda crappy that I'm assuming ancestor *is* in fact an
+        # ancestor. It is for my usage, but again, a general structure would
+        # need to define a protocol for handling that - return -1, throw
+        # exception, etc
+        test_node = self
+        while test_node is not ancestor:
+            test_node = test_node.parent
+            depth += 1
+
+        return depth
+
+
     def count_orbits(self):
         count = 0
 
@@ -89,6 +128,14 @@ def find_root_node(objects):
         if obj.parent is None:
             return obj
 
+def find_common_ancestor(left_node, right_node):
+    left_ancestor = left_node
+
+    while not left_ancestor.contains(right_node):
+        left_ancestor = left_ancestor.parent
+
+    return left_ancestor
+
 
 def count_orbits(input_path):
     system_objects = parse_system_objects(input_path)
@@ -103,5 +150,22 @@ def count_orbits(input_path):
     print(root_node.count_orbits())
 
 
+def count_orbital_transfers_to_santa(input_path):
+    system_objects = parse_system_objects(input_path)
+
+    my_node = system_objects['YOU']
+    santa_node = system_objects['SAN']
+
+    common_ancestor = find_common_ancestor(my_node, santa_node)
+
+    transfers_to_common_ancestor = my_node.depth(common_ancestor) - 1
+
+    transfers_to_santa = santa_node.depth(common_ancestor) - 1
+
+    return transfers_to_common_ancestor + transfers_to_santa
+
+
 if __name__ == '__main__':
-    count_orbits(sys.argv[1])
+    result = count_orbital_transfers_to_santa(sys.argv[1])
+
+    print(result)
